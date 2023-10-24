e240938f8151e809352aa9760e548c7c53442072
Ashley Grant <agrant2@nic.edu>
2023-10-24 15:29:14 -0700

    made nursery rhyme

---
+++ script.js
@@ -0,0 +1,12 @@
+// we are creating 5 Little Ducks
+
+var count = 10;
+
+while (count > 2) {
+    console.log(count + " little ducks went swimming one day. Over the hills and far away. Mother Duck said QUACK QUACK QUACK QUACK! But only " + (count - 1) + " little ducks came back!");/*concatenation is the +++ stuff in string above*/
+    /*count--;*/
+    count = count - 1;
+    }
+
+    console.log("No little ducks went swimming one day. Over the hills and far away. Mother Duck Said Quack Quack Quack Quack! And all five little ducks came back!");
+
